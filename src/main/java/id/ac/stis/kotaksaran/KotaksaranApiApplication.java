1e80cd76be441414d94479df12b9a83c20f5bf72
Amir <222312968@stis.ac.id>
2025-10-25 23:17:36 +0700

    clean dan siap build :)

--- src/main/java/id/ac/stis/kotaksaran/KotaksaranApiApplication.java
+++ src/main/java/id/ac/stis/kotaksaran/KotaksaranApiApplication.java
@@ -2,15 +2,18 @@ package id.ac.stis.kotaksaran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories; 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "id.ac.stis.kotaksaran") 
-@EntityScan(basePackages = "id.ac.stis.kotaksaran")
+@ComponentScan(basePackages = "id.ac.stis.kotaksaran")
+@EnableJpaRepositories(basePackages = "id.ac.stis.kotaksaran.repository")
+@EntityScan(basePackages = "id.ac.stis.kotaksaran.entity")
 public class KotaksaranApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KotaksaranApiApplication.class, args);
     }
 }
+
