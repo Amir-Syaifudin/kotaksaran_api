271714cf209c77a55f4e307f0c3d4038a58afc4d
Amir <222312968@stis.ac.id>
2025-10-25 14:34:52 +0700

    semoga last

--- src/main/java/id/ac/stis/kotaksaran/KotaksaranApiApplication.java
+++ src/main/java/id/ac/stis/kotaksaran/KotaksaranApiApplication.java
@@ -1,20 +1,16 @@
-package id.ac.stis.kotaksaran;
+package id.ac.stis.kotaksaran; 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories; 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "id.ac.stis.kotaksaran.repository")
-@EntityScan(basePackages = "id.ac.stis.kotaksaran.entity")
+@EnableJpaRepositories(basePackages = "id.ac.stis.kotaksaran") 
+@EntityScan(basePackages = "id.ac.stis.kotaksaran")
 public class KotaksaranApiApplication {
 
-  public static void main(String[] args) {
-    System.setProperty("spring.devtools.restart.enabled", "false");
-    System.setProperty("spring.jpa.show-sql", "true");
-    System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-    System.setProperty("spring.jpa.hibernate.ddl-auto", "update"); // Assign a valid String value here
-    SpringApplication.run(KotaksaranApiApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(KotaksaranApiApplication.class, args);
+    }
 }
